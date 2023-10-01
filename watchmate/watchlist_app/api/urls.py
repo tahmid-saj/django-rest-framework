6486c5d03847ab244379ba3bf7189f794abc8f79
tahmid-saj <tahmid.saj@gmail.com>
2023-10-01 13:40:33 -0400

    url structure for stream

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
-    path('review', ReviewList.as_view(), name="review-list"),
-    path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
+
+    path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name="stream-detail"),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
+    
+    # path('review', ReviewList.as_view(), name="review-list"),
 ]
