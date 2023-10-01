f6f744e3a9ae4b3207f95c4a217d6bd811a85fe9
tahmid-saj <tahmid.saj@gmail.com>
2023-09-30 21:50:09 -0400

    404 / 400 status code

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,6 +1,7 @@
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -29,14 +30,18 @@ def movie_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
   if request.method == 'GET':
-    movie = Movie.objects.get(pk=pk)  
+    try:
+      movie = Movie.objects.get(pk=pk)  
+    except Movie.DoesNotExist:
+      return Response({ 'Error': 'Movie not found' }, status=status.HTTP_404_NOT_FOUND)
+
     serializer = MovieSerializer(movie)
 
     return Response(serializer.data)
   
   if request.method == 'PUT':
     movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie, data=request.data)
+    serializer = MovieSerializer(movie, data=request.data, status=status.HTTP_400_BAD_REQUEST)
 
     if serializer.is_valid():
       serializer.save()
@@ -48,3 +53,5 @@ def movie_details(request, pk):
   if request.method == 'DELETE':
     movie = Movie.objects.get(pk=pk)
     movie.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
