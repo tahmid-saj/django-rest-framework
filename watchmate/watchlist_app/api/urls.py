8b3d27215160412e2d2d880baa4d4c12d74fd2b2
tahmid-saj <tahmid.saj@gmail.com>
2023-10-01 15:05:53 -0400

    class viewset

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 # from watchlist_app.api.views import movie_list, movie_details
 from watchlist_app.api.views import (WatchListAV, WatchDetailAV, StreamPlatformAV, 
-                                     StreamPlatformDetailAV, ReviewList, ReviewDetail, ReviewCreate)
+                                     StreamPlatformDetailAV, ReviewList, ReviewDetail, 
+                                     ReviewCreate, StreamPlatformVS)
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
+
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
-    path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+    # path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
+    # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+
+    path('', include(router.urls)),
 
     # path('review/', ReviewList.as_view(), name="review-list"),
     # path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -2,13 +2,15 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.serializers import (WatchListSerializer, StreamPlatformSerializer, 
                                           ReviewSerializer)
 
-from rest_framework import status, generics, mixins
+from rest_framework import status, generics, mixins, viewsets
 
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from django.http import JsonResponse
+
 
 class ReviewCreate(generics.CreateAPIView):
   serializer_class = ReviewSerializer
@@ -51,6 +53,22 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
   
 #   def post(self, request, *args, **kwargs):
 #     return self.create(request, *args, **kwargs)
+
+
+class StreamPlatformVS(viewsets.ViewSet):
+
+  def list(self, request):
+    queryset = StreamPlatform.objects.all()
+    serializer = StreamPlatformSerializer(queryset, many=True)
+
+    return Response(serializer.data)
+
+  def retrieve(self, request, pk):
+    queryset = StreamPlatform.objects.all()
+    watchlist = get_object_or_404(queryset, pk=pk)
+    serializer = StreamPlatformSerializer(StreamPlatform)
+
+    return Response(serializer.data)
 
 
 class StreamPlatformAV(APIView):
