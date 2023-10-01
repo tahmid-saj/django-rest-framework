7bdaf29e2083706a8aa4d97f2d4c40d565916d79
tahmid-saj <tahmid.saj@gmail.com>
2023-09-30 20:04:48 -0400

    movie list view

--- watchmate/watchlist_app/urls.py
+++ watchmate/watchlist_app/urls.py
@@ -1,10 +1,6 @@
-from django.db import models
+from django.urls import path, include
+from watchlist_app.views import movie_list
 
-# Create your models here.
-class Movie(models.Model):
-  name = models.CharField(max_length=50)
-  description = models.CharField(max_length=200)
-  active = models.BooleanField(default=True)
-
-  def __str__(self):
-    return self.name
+urlpatterns = [
+    path('list/', movie_list, name='movie-list'),
+]
