15d734f09f44494cee32f4f37fb0444b0094fefe
tahmid-saj <tahmid.saj@gmail.com>
2023-10-08 17:53:07 -0400

    basic auth

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -12,7 +12,7 @@ router.register('stream', StreamPlatformVS, basename='streamplatform')
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
-    path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
+    path('<int:pk>/', WatchDetailAV.as_view(), name='movie-details'),
     # path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
     # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
 
@@ -21,9 +21,9 @@ urlpatterns = [
     # path('review/', ReviewList.as_view(), name="review-list"),
     # path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
 
-    path('<int:pk>/reviews', ReviewList.as_view(), name="review-list"),
-    path('<int:pk>/review-create', ReviewCreate.as_view(), name="review-create"),
-    path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
+    path('<int:pk>/reviews/', ReviewList.as_view(), name="review-list"),
+    path('<int:pk>/review-create/', ReviewCreate.as_view(), name="review-create"),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
     
     # path('review', ReviewList.as_view(), name="review-list"),
 ]
