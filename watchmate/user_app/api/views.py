5937984129833e12718d089cb9aa988e981c3aaa
tahmid-saj <tahmid.saj@gmail.com>
2023-10-09 02:01:48 -0400

    jwt auth register

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from user_app.api.serializers import RegistrationSerializer
 from user_app import models
@@ -29,6 +30,13 @@ def registration_view(request):
       data['username'] = account.username
       data['email'] = account.email
       data['token'] = Token.objects.get(user=account).key
+
+      refresh = RefreshToken.for_user(user=(account))
+      data['token'] = {
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token)
+                      }
+      
     else:
       data = serializer.errors
       
