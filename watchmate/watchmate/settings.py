9138fadddb4eeb1cddbbd4a6e8c5c9d2aaebe9ff
tahmid-saj <tahmid.saj@gmail.com>
2023-10-09 00:33:37 -0400

    jwt auth

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -134,6 +134,8 @@ REST_FRAMEWORK = {
 #     "rest_framework.authentication.BasicAuthentication",
 #   ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-    "rest_framework.authentication.TokenAuthentication",
-  ]
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ]
 }
