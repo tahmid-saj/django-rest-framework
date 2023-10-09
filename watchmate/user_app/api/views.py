067bcd53e42c0058dd4ef77517ff3a765effdd25
tahmid-saj <tahmid.saj@gmail.com>
2023-10-08 20:40:30 -0400

    token auth

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 from user_app.api.serializers import RegistrationSerializer
+from user_app import models
+
 
 @api_view(['POST',])
 def registration_view(request):
@@ -9,8 +12,17 @@ def registration_view(request):
   if request.method == 'POST':
     serializer = RegistrationSerializer(data=request.data)
 
-    if serializer.is_valid():
-      serializer.save()
+    data = {}
 
-      return Response(serializer.data) 
+    if serializer.is_valid():
+      account = serializer.save()
+
+      data['response'] = "Registration successful"
+      data['username'] = account.username
+      data['email'] = account.email
+      data['token'] = Token.objects.get(user=account).key
+    else:
+      data = serializer.errors
+      
+    return Response(data) 
 
